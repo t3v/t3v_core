8d5d2d357daaec3c8f9af5b81246afcfaf4918ec
Maik Kempe <mkempe@bitaculous.com>
2016-07-14 16:50:48 +0200

    [✚] Added `AbstractRepository`.

---
+++ Classes/Domain/Repository/AbstractRepository.php
@@ -0,0 +1,85 @@
+<?php
+namespace T3v\T3vCore\Domain\Repository;
+
+use \TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
+use \TYPO3\CMS\Extbase\Persistence\QueryInterface;
+use \TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * Class AbstractRepository
+ *
+ * @package T3v\T3vCore\Domain\Repository
+ */
+abstract class AbstractRepository extends Repository {
+  protected $defaultOrderings = array(
+    'crdate' => QueryInterface::ORDER_DESCENDING,
+    'uid'    => QueryInterface::ORDER_DESCENDING
+  );
+
+  /**
+   * Life cycle method.
+   *
+   * @return void
+   */
+  public function initializeObject() {
+    $querySettings = $this->objectManager->get('TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings');
+    $querySettings->setIgnoreEnableFields(false);
+    $querySettings->setRespectStoragePage(false);
+
+    $this->setDefaultQuerySettings($querySettings);
+  }
+
+  /**
+   * Finder to query by multiple UIDs.
+   *
+   * @param array $uids The UIDs
+   * @param init $limit The limit
+   * @return array The found objects
+   */
+  public function findByUids($uids) {
+    $query = $this->createquery();
+
+    // Adjust query settings
+    // $query->getQuerySettings()->setRespectStoragePage(false);
+    // $query->getQuerySettings()->setReturnRawQueryResult(true);
+
+    // Built up constraints
+    $constraints = array(
+      $query->equals('deleted', 0),
+      $query->equals('hidden', 0)
+    );
+
+    if (!empty($uids)) {
+      $constraints[] = $query->in('uid', $uids);
+    }
+
+    // Set constraints
+    $query->matching($query->logicalAnd($constraints));
+
+    // Set limit if available
+    if (!empty($limit)) {
+      $query->setLimit($limit);
+    }
+
+    $query->setOrderings($this->orderByField('uid', $uids));
+
+    return $query->execute();
+  }
+
+  /**
+   * Helper to get the orderings by field.
+   *
+   * @param string $field The field
+   * @param array $values The values
+   * @return array The orderings
+   */
+  protected function orderByField($field, $values) {
+    $orderings = array();
+
+    foreach ($values as $value) {
+      $orderings["$field={$value}"] = QueryInterface::ORDER_DESCENDING;
+    }
+
+    return $orderings;
+  }
+}
