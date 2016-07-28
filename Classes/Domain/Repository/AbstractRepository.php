1d89d1dc2befb50b1549541563be675fa8729e88
Maik Kempe <mkempe@bitaculous.com>
2016-07-28 06:47:06 +0200

    [➦] Merged changes.

--- Classes/Domain/Repository/AbstractRepository.php
+++ Classes/Domain/Repository/AbstractRepository.php
@@ -11,10 +11,10 @@ use \TYPO3\CMS\Extbase\Persistence\Repository;
  * @package T3v\T3vCore\Domain\Repository
  */
 abstract class AbstractRepository extends Repository {
-  protected $defaultOrderings = array(
+  protected $defaultOrderings = [
     'crdate' => QueryInterface::ORDER_DESCENDING,
     'uid'    => QueryInterface::ORDER_DESCENDING
-  );
+  ];
 
   /**
    * Life cycle method.
@@ -34,7 +34,7 @@ abstract class AbstractRepository extends Repository {
    *
    * @param array $uids The UIDs
    * @param int $limit The optional limit
-   * @return array The found objects
+   * @return \Extbase\Persistence\QueryResult The found objects
    */
   public function findByUids($uids, $limit = null) {
     $query = $this->createquery();
@@ -67,11 +67,25 @@ abstract class AbstractRepository extends Repository {
   }
 
   /**
-   * Finder to query by multiple PIDs.
+   * Finder to query by page ID.
    *
-   * @param array $pids The PIDs
+   * @param int $pid The page ID
+   * @return mixed The found object or null if no object was found
+   */
+  public function findByPid($pid) {
+    $query = $this->createQuery();
+
+    $query->matching($query->equals('pid', $pid));
+
+    return $query->execute()->getFirst();
+  }
+
+  /**
+   * Finder to query by multiple page IDs.
+   *
+   * @param array $pids The page IDs
    * @param int $limit The optional limit
-   * @return array The found objects
+   * @return \Extbase\Persistence\QueryResult The found objects
    */
   public function findByPids($pids, $limit = null) {
     $query = $this->createquery();
@@ -111,7 +125,7 @@ abstract class AbstractRepository extends Repository {
    * @return array The orderings
    */
   protected function orderByField($field, $values) {
-    $orderings = array();
+    $orderings = [];
 
     foreach ($values as $value) {
       $orderings["$field={$value}"] = QueryInterface::ORDER_DESCENDING;
