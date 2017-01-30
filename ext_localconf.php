3de3036a11a33772be62e39af74c4390429cabb9
Maik Kempe <mkempe@bitaculous.com>
2017-01-30 21:00:13 +0100

    [➠] Use `call_user_func` in `ext_localconf.php` and `ext_tables.php`.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,9 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied.');
 
-// === TCA Configuration ===
+call_user_func(function ($extkey) {
+  // === TCA Configuration ===
 
-// Add `backend_layout` and `backend_layout_next_level` field to the root line.
-$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',backend_layout,backend_layout_next_level';
+  // Add `backend_layout` and `backend_layout_next_level` field to the root line.
+  $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',backend_layout,backend_layout_next_level';
+}, $_EXTKEY);
--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,8 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied.');
 
-// === TypoScript ===
+call_user_func(function ($extkey) {
+  // === TypoScript ===
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'T3v Core');
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'T3v Core');
+}, $_EXTKEY);
