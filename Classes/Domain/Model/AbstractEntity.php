40e731403aa4ad2444cd530906e66e1d25ab3fa7
Maik Kempe <mkempe@bitaculous.com>
2017-01-31 12:40:16 +0100

    [➠] Added `crdate` to `AbstractEntity`.

--- Classes/Domain/Model/AbstractEntity.php
+++ Classes/Domain/Model/AbstractEntity.php
@@ -8,11 +8,27 @@ namespace T3v\T3vCore\Domain\Model;
  */
 abstract class AbstractEntity extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
   /**
+   * The entitie's creation date.
+   *
+   * @var DateTime
+   */
+  protected $crdate;
+
+  /**
    * The entitie's system language UID.
    *
    * @var int
    */
   protected $sysLanguageUid;
+
+  /**
+   * Returns the entitie's creation date.
+   *
+   * @return DateTime The entitie's creation date
+   */
+  public function getCrdate() {
+    return $this->crdate;
+  }
 
   /**
    * Returns the entitie's system language UID.
