42421a89c5832fce25455cd2c5773d5a650ef5dc
Maik Kempe <mkempe@bitaculous.com>
2017-01-31 14:17:07 +0100

    [➠] Updated `ext_tables.php`.

--- ext_tables.php
+++ ext_tables.php
@@ -4,5 +4,5 @@ defined('TYPO3_MODE') or die('Access denied.');
 call_user_func(function ($extkey) {
   // === TypoScript ===
 
-  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'T3v Core');
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extkey, 'Configuration/TypoScript', 'T3v Core');
 }, $_EXTKEY);
