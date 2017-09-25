addb5dbe30d28d6814264020d820094fc159140a
Maik Kempe <mkempe@bitaculous.com>
2017-09-25 23:24:47 +0200

    [✓] Fixed imports.

--- Classes/Utility/RendererUtility.php
+++ Classes/Utility/RendererUtility.php
@@ -5,7 +5,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
-use T3v\T3vCore\Utility\AbstractHelper;
+use T3v\T3vCore\Utility\AbstractUtility;
 
 /**
  * Renderer Utility Class
