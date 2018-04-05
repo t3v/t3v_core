f9121593b7591a2b336bc0f41ed599d45e54bda0
Maik Kempe <mkempe@bitaculous.com>
2018-04-05 15:12:08 +0200

    [♻] Cleaned up.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-// === Frontend Configuration ===
+// === Frontend ===
 
 // Add `backend_layout` and `backend_layout_next_level` field to the root line.
 $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',backend_layout,backend_layout_next_level';
