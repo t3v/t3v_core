86b1ee9b05478e31a229f8aba2bf5557609a553a
Maik Kempe <mkempe@bitaculous.com>
2018-04-13 00:31:00 +0200

    [➠] Improved `AbstractEntity`.

--- Classes/Domain/Model/AbstractEntity.php
+++ Classes/Domain/Model/AbstractEntity.php
@@ -17,11 +17,32 @@ abstract class AbstractEntity extends AbstractEntityExtbase {
   protected $crdate;
 
   /**
+   * The entitie's timestamp.
+   *
+   * @var \DateTime
+   */
+  protected $tstamp;
+
+  /**
    * The entitie's system language UID.
    *
    * @var int
    */
   protected $sysLanguageUid;
+
+  /**
+   * The entitie's L10n parent.
+   *
+   * @var int
+   */
+  protected $l10nParent;
+
+  /**
+   * The entitie's localized UID.
+   *
+   * @var int
+   */
+  protected $_localizedUid;
 
   /**
    * Returns the entitie's creation date.
@@ -33,11 +54,38 @@ abstract class AbstractEntity extends AbstractEntityExtbase {
   }
 
   /**
+   * Returns the entitie's timestamp.
+   *
+   * @return \DateTime The entitie's timestamp
+   */
+  public function getTstamp() {
+    return $this->tstamp;
+  }
+
+  /**
    * Returns the entitie's system language UID.
    *
    * @return int The entitie's system language UID
    */
   public function getSysLanguageUid() {
     return $this->sysLanguageUid;
+  }
+
+  /**
+   * Returns the entitie's L10n parent.
+   *
+   * @return int The entitie's L10n parent
+   */
+  public function getL10nParent() {
+    return $this->l10nParent;
+  }
+
+  /**
+   * Returns the entitie's localized UID.
+   *
+   * @return int The entitie's localized UID
+   */
+  public function getLocalizedUid() {
+    return $this->_localizedUid;
   }
 }
