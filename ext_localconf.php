7d8241b06e13d7444827d47faccc3802a2d15ae0
Maik Kempe <mkempe@bitaculous.com>
2020-09-15 13:22:42 +0200

    [➠] Updated dependencies. [➠] Updated Travis CI configuration. [♻] Cleaned up.

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,7 @@ defined('TYPO3_MODE') or die();
 
 // Adds the `backend_layout` and `backend_layout_next_level` field to the root line.
 if (empty($GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'])) {
-  $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= 'backend_layout,backend_layout_next_level';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= 'backend_layout,backend_layout_next_level';
 } else {
-  $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',backend_layout,backend_layout_next_level';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',backend_layout,backend_layout_next_level';
 }
