7301173edb0c350d35996a0b09d8dc134fba9a4a
Jochen Greiner <jochen@greiner.de>
2021-12-20 17:00:42 +0100

    fallback

---
+++ Classes/Service/LanguageService.php
@@ -0,0 +1,15 @@
+<?php
+namespace T3v\T3vCore\Service;
+
+
+/**
+ * Class LanguageService
+ * @package T3v\T3vCore\Service
+ * @author Jochen Greiner
+ * @deprecated use LocalizationService directly
+ * Date: 20.12.21
+ * Time: 16:59
+ */
+class LanguageService extends LocalizationService {
+
+}
