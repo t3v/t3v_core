cbb7de89c52df6e6ec78bb8403793f814a608c2c
Maik Kempe <mkempe@bitaculous.com>
2022-01-18 14:25:08 +0100

    [➠] Updated comments [ci skip].

--- Classes/Validation/Validator/NoValidator.php
+++ Classes/Validation/Validator/NoValidator.php
@@ -10,10 +10,10 @@ namespace T3v\T3vCore\Validation\Validator;
 class NoValidator extends AbstractValidator
 {
     /**
-     * Checks if a value is valid, returns always true.
+     * Checks if the value is valid, always returns true.
      *
      * @param mixed $value The value to validate
-     * @return bool If the value is valid, returns always true
+     * @return bool Always returns true
      */
     public function isValid($value): bool
     {
