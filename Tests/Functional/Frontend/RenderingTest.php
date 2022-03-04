207214dc58100320e6b39d5cc53b07f884d0bb81
Maik Kempe <mkempe@bitaculous.com>
2022-03-04 14:58:55 +0100

    [➠] Improved tests.

--- Tests/Functional/Frontend/RenderingTest.php
+++ Tests/Functional/Frontend/RenderingTest.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace T3v\T3vCore\Tests\Functional\Frontend;
 
+use Doctrine\DBAL\DBALException;
 use T3v\T3vTesting\Tests\Functional\Frontend\Traits\SetupTrait;
+use TYPO3\TestingFramework\Core\Exception;
 use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
@@ -35,8 +37,8 @@ class RenderingTest extends FunctionalTestCase
      * @var array
      */
     protected $testExtensionsToLoad = [
-        'typo3conf/ext/t3v_core',
-        'typo3conf/ext/t3v_testing'
+        'typo3conf/ext/t3v_testing',
+        'typo3conf/ext/t3v_core'
     ];
 
     /**
@@ -44,8 +46,7 @@ class RenderingTest extends FunctionalTestCase
      *
      * @var array
      */
-    protected $pathsToLinkInTestInstance = [
-    ];
+    protected $pathsToLinkInTestInstance = [];
 
     /**
      * Tests if the template is rendered.
@@ -66,7 +67,7 @@ class RenderingTest extends FunctionalTestCase
             $titleTag = $xpath->query('/html/head/title')->item(0);
             $generatorMetaTag = $xpath->query('/html/head/meta[@name="generator"]')->item(0);
 
-            self::assertEquals('Home | T3v Core', $titleTag->nodeValue);
+            self::assertStringContainsString('Home', $titleTag->nodeValue);
             self::assertEquals('TYPO3 CMS', $generatorMetaTag->getAttribute('content'));
         }
     }
@@ -74,9 +75,8 @@ class RenderingTest extends FunctionalTestCase
     /**
      * Setup before running tests.
      *
-     * @throws \Doctrine\DBAL\DBALException
-     * @throws \TYPO3\TestingFramework\Core\Exception
-     * @noinspection PhpFullyQualifiedNameUsageInspection
+     * @throws DBALException
+     * @throws Exception
      */
     protected function setUp(): void
     {
