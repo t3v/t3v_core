df45d9a4dc35afb683cb1f3155425954250ffc45
Maik Kempe <mkempe@bitaculous.com>
2022-03-10 00:40:35 +0100

    [➠] Updated comments [ci skip].

--- Classes/Service/LanguageService.php
+++ Classes/Service/LanguageService.php
@@ -7,7 +7,7 @@ namespace T3v\T3vCore\Service;
  * The language service class.
  *
  * @package T3v\T3vCore\Service
- * @deprecated Use `LocalizationService` directly, will be removed in a next major version
+ * @deprecated Use `T3v\T3vCore\Service\LocalizationService` directly, will be removed in a next major version.
  */
 class LanguageService extends LocalizationService {
 
