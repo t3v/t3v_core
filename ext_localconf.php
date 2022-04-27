c002d8df214efbac48c43ecca3c94047e13708c8
Maik Kempe <mkempe@bitaculous.com>
2022-04-27 14:12:50 +0200

    [➦] Merged changes.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') or die();
 
 // === Frontend ===
 
-// Adds the `backend_layout` and `backend_layout_next_level` field to the root line.
+// Adds the `backend_layout` and `backend_layout_next_level` field to the root line:
 if (empty($GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'])) {
   $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= 'backend_layout,backend_layout_next_level';
 } else {
